352bb7d279b8b463bb5b642d45bd5590e615cf28
changuk63 <changuk63@naver.com>
2016-04-02 02:31:36 +0900

    PlanRates modified

--- phonebill/src/main/java/com/team11/phonebill/PlanRates.java
+++ phonebill/src/main/java/com/team11/phonebill/PlanRates.java
@@ -1,7 +1,11 @@
 package com.team11.phonebill;
 
 public abstract class PlanRates {
-	
+	private double basicMontlyRate;
+	private double ratePerExcessMinute;
+	private int includeMinutes;
+	private double additionalLineRate;
+
 	public abstract double getBasicMonthlyRate();
 	
 	public abstract int getIncludedMinutes();
